348c9d945e968a7c8a66780b24a8add0d44f3009
David Ohayon <ohayon.1@gmail.com>
2012-11-27 20:26:26 -0500

    ajax working to pull in all offers to ui-listview

--- www/js/offers.js
+++ www/js/offers.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
+	//---- GET offers.json into a ui-listview
 	$.getJSON('http://localhost:3000/offers.json', function(data){
 		$.each(data, function(key, value){
-			$('.ui-listview').append("<li class='ui-li ui-li-static ui-btn-up-c ui-li-last'>" + value.name + "</li>");
+			$('.ui-listview').append("<li class='ui-li ui-li-static ui-btn-up-c ui-li-last'>" + value.name + ".... points: " + value.point_value + "</li>");
 		});	
 	});
-	
-	// $('.ui-listview').append("<li>This is a Test</li>")
-
 	$('.ui-listview').listview('refresh');
 
 });
