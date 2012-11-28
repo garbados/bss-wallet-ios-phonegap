5b301d52dc1b7e505149cca93b7d32b8fd8657e4
David Ohayon <ohayon.1@gmail.com>
2012-11-27 23:55:29 -0500

    messing around with js a bit.

--- www/js/offers.js
+++ www/js/offers.js
@@ -1,11 +1,17 @@
+//---- DOCUMENT READY
 $(document).ready(function() {
 
 	//---- GET offers.json into a ui-listview
-	$.getJSON('http://localhost:3000/offers.json', function(data){
+	var jsonOffersList = $.getJSON('http://localhost:3000/offers.json', function(data){
 		$.each(data, function(key, value){
-			$('.ui-listview').append("<li class='ui-li ui-li-static ui-btn-up-c ui-li-last'>" + value.name + ".... points: " + value.point_value + "</li>");
+			$('.ui-listview').append("<li data-theme='c' class='ui-li ui-li-static ui-btn-up-c ui-li-last'>" + value.name + ".... points: " + value.point_value + "</li>");
 		});	
 	});
 	$('.ui-listview').listview('refresh');
+	alert();
+	//---- view details of offer
+	$('.ui-li-static').click(function(){
+		$(this).parent().slideDown();
+	});
 
 });
