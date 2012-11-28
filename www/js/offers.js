a6990007f818f18e2aafd215080f02e17c16ff5f
David Ohayon <ohayon.1@gmail.com>
2012-11-27 19:34:17 -0500

    got <li> items working. it needs the full set of classes jQuery creates while looping.

--- www/js/offers.js
+++ www/js/offers.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
 
 	$.getJSON('http://localhost:3000/offers.json', function(data){
 		$.each(data, function(key, value){
-			$('ul').append("<li>" + value.name + "</li>");
+			$('.ui-listview').append("<li class='ui-li ui-li-static ui-btn-up-c ui-li-last'>" + value.name + "</li>");
 		});	
 	});
+	
+	// $('.ui-listview').append("<li>This is a Test</li>")
+
+	$('.ui-listview').listview('refresh');
+
 });
